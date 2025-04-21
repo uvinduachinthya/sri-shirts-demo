efcd7f66ec61df65b26b8fca47f1859b22c1e266
Uvindu Achinthya <uvinduachinthya@gmail.com>
2025-04-22 02:39:42 +0530

    updated for pages

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/sri-shirts-demo",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
